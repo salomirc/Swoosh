e3c7dad03636a514cf29e137cd65aca1c2a6d917
salomirc <36732879+salomirc@users.noreply.github.com>
2019-11-25 18:27:55 +0200

    Lesson 2 commit 1.

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -2,11 +2,17 @@ package com.example.swoosh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_league.*
+import kotlinx.android.synthetic.main.activity_welcome.*
+import java.util.*
 
 class LeagueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+        val nextButtonUppercase = NextButton.text.toString().toUpperCase(Locale.getDefault())
+        NextButton.text = nextButtonUppercase
     }
 }
--- app/src/main/java/com/example/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/example/swoosh/WelcomeActivity.kt
@@ -1,12 +1,25 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ShareCompat
+import kotlinx.android.synthetic.main.activity_welcome.*
+import java.util.*
+import kotlin.reflect.typeOf
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        val getStartedUppercase = GetStartedButton.text.toString().toUpperCase(Locale.getDefault())
+        GetStartedButton.text = getStartedUppercase
+
+        GetStartedButton.setOnClickListener {
+            var intent = Intent(this, LeagueActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
